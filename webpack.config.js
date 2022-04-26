51c5fe9212abbd65282a08d400182847902603e2
Перцев Денис <thech@yandex.ru>
2022-04-26 23:16:22 +0300

    [200~Настройка сборки

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    clean: true,
+    devtool: 'source-map',
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: 'public' }],
+      }),
+    ],
+  },
+};
