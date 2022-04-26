1fd0adce0b2070e8cb3c447f8fea0851957639d7
Перцев Денис <thech@yandex.ru>
2022-04-26 23:45:34 +0300

    Настройка сервера для разработки

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     clean: true,
-    devtool: 'source-map',
-    plugins: [
-      new CopyPlugin({
-        patterns: [{ from: 'public' }],
-      }),
-    ],
   },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
 };
