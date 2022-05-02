cec7b551129f5fc6720109a4c296f0d2453b576e
Перцев Денис <thech@yandex.ru>
2022-05-02 14:02:24 +0300

    Добавил константы для генерации демо-данных

--- src/const.js
+++ src/const.js
@@ -1,1 +1,15 @@
 export const POINTS_COUNT = 3;
+export const PHOTO_COUNT = 100;
+export const MIN_PRICE = 100;
+export const MAX_PRICE = 10000;
+export const POINT_TYPES = [
+  'taxi',
+  'bus',
+  'train',
+  'ship',
+  'drive',
+  'flight',
+  'check-in',
+  'sightseeing',
+  'restaurant',
+];
