d888ce7cdf215f49d1cc586106f750053e9ee2b1
Перцев Денис <thech@yandex.ru>
2022-06-08 13:54:53 +0300

    Loader --> формирование идентификатора ресурса и парам.запроса вынес в отдельные методы

--- src/services/loader.js
+++ src/services/loader.js
@@ -22,6 +22,17 @@ export default class Loader {
     this.#pointsURN = pointsURN;
   }
 
+  #getApiUri = (name) => `${this.#serverURL}${name}`;
+
+  #getParams = (method = Method.GET, contentType=undefined, body = undefined)=>({
+    method,
+    headers:{
+      authorization: 'Basic hS2sfS44wcl1sa2j',
+      'Content-Type': contentType
+    },
+    body
+  });
+
   #sendRequest = async (uri, params) => {
     const response = await fetch(uri, params);
     if (response.ok) {
@@ -33,51 +44,29 @@ export default class Loader {
   };
 
   getPoints = async (loadPointsHandler) => {
-    const uri = `${this.#serverURL}${this.#pointsURN}`;
-    const params = {
-      method: Method.GET,
-      headers: {
-        Authorization: 'Basic hS2sfS44wcl1sa2j',
-      },
-    };
+    const uri = this.#getApiUri(this.#pointsURN);
+    const params = this.#getParams();
     const response = await this.#sendRequest(uri, params);
     loadPointsHandler(response);
   };
 
   getDestinations = async (loadDestinationsHandler) => {
-    const uri = `${this.#serverURL}${this.#destinationsURN}`;
-    const params = {
-      method: Method.GET,
-      headers: {
-        Authorization: 'Basic hS2sfS44wcl1sa2j',
-      },
-    };
+    const uri = this.#getApiUri(this.#destinationsURN);
+    const params = this.#getParams();
     const response = await this.#sendRequest(uri, params);
     loadDestinationsHandler(response);
   };
 
   getOffers = async (loadOffersHandler) => {
-    const uri = `${this.#serverURL}${this.#offersURN}`;
-    const params = {
-      method: Method.GET,
-      headers: {
-        Authorization: 'Basic hS2sfS44wcl1sa2j',
-      },
-    };
+    const uri = this.#getApiUri(this.#offersURN);
+    const params = this.#getParams();
     const response = await this.#sendRequest(uri, params);
     loadOffersHandler(response);
   };
 
   updatePoint = async (point, updatePointHandler, args) => {
-    const uri = `${this.#serverURL}${this.#pointsURN}/${point.id}`;
-    const params = {
-      method: Method.PUT,
-      headers: {
-        Authorization: 'Basic hS2sfS44wcl1sa2j',
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(point)
-    };
+    const uri = this.#getApiUri(`${this.#pointsURN}/${point.id}`);
+    const params = this.#getParams(Method.PUT, 'application/json;charset=utf-8', JSON.stringify(point));
     const response = await this.#sendRequest(uri, params);
     updatePointHandler(response, args);
   };
